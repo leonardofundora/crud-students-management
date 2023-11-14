e1a2d9d5c70eca13523aea7316fd168e2f583fcf
Leonardo Fundora <127889085+leonardofundora@users.noreply.github.com>
2023-11-13 21:18:31 -0500

    chore: fix sequelize config

--- config/database.js
+++ config/database.js
@@ -1,11 +1,13 @@
-import Sequelize from "sequelize";
+const Sequelize = require("sequelize");
 
-export const sequelize = new Sequelize(
+const sequelize = new Sequelize(
     process.env.DB_NAME || "postgres",
     process.env.DB_USER || "postgres",
     process.env.DB_PASSWORD || "postgres",
     {
         host: process.env.DB_HOST || "localhost",
-        dialect: "postgres",
+        dialect: process.env.DB_DIALECT || "postgres",
     },
 );
+
+module.export(sequelize);
